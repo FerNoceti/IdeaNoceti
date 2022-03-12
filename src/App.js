f894add02ed12b14232fe4405e5c568fc25ccea8
FerNoceti <fer.j.noceti@gmail.com>
2022-03-12 20:23:26 -0300

    Menú e-commerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tienda de prueba
-        </p>
-        <a
-          className="App-link"
-          href="https://mercadolibre.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Compra ahora
-        </a>
-      </header>
+        <NavBar />
     </div>
   );
 }
